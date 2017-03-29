aeac56fa26bd4d90486a67177f36d5ec479ffe70
Edd Smith <edd@sourcelab.technology>
2017-03-29 11:12:49 +0100

    did a thing

---
+++ Heartrate_intensity.rb
@@ -0,0 +1,27 @@
+@intensity = 0.55
+
+def get_target_heart_rate(intensity)
+  target_heart_rate = (((220 - @age) - @resting_heart_rate) * intensity) + @resting_heart_rate
+  print intensity.round(2)
+
+  puts '      | ' + target_heart_rate.round.to_s
+  intensity + 0.05
+
+  if intensity < 0.95
+    get_target_heart_rate(intensity + 0.05)
+  end
+end
+
+puts 'Please enter you age:'
+
+@age = gets.chomp.to_i
+
+puts 'Please enter your resting heart rate:'
+
+@resting_heart_rate = gets.chomp.to_i
+
+puts ''
+puts 'Intensity | Target Heart Rate'
+puts '--------------------------'
+
+get_target_heart_rate(@intensity)
