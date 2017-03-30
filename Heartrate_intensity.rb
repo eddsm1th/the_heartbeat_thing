010c5548a7efbccc8dcf9db3e98c09e5da9c4fe0
Edd Smith <edd@sourcelab.technology>
2017-03-30 11:25:55 +0100

    did a thing

--- Heartrate_intensity.rb
+++ Heartrate_intensity.rb
@@ -1,1 +1,10 @@
-def get_target_heart_rate(intensity); target_heart_rate = (((220 - @age) - @resting_heart_rate) * intensity) + @resting_heart_rate; print ((intensity*100).to_i).to_s + '%' + '       | ' + target_heart_rate.round.to_s + "\n"; intensity + 0.05; get_target_heart_rate(intensity + 0.05) if intensity < 0.95; end; puts 'Please enter you age:'; @age = gets.chomp.to_i; puts 'Please enter your resting heart rate:'; @resting_heart_rate = gets.chomp.to_i; puts "\nIntensity | Target Heart Rate\n----------|--------------"; get_target_heart_rate(0.55)
+def get_target_heart_rate(intensity)
+  target_heart_rate = (((220 - @age) - @resting_heart_rate) * intensity) + @resting_heart_rate
+  print ((intensity*100).to_i).to_s + '%' + '       | ' + target_heart_rate.round.to_s + "\n"
+  intensity + 0.05
+  get_target_heart_rate(intensity + 0.05) if intensity < 0.95
+end
+puts 'Please enter you age:'; @age = gets.chomp.to_i
+puts 'Please enter your resting heart rate:'; @resting_heart_rate = gets.chomp.to_i
+puts "\nIntensity | Target Heart Rate\n----------|--------------"
+get_target_heart_rate(0.55)
