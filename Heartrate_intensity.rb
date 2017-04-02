c1bd6407483f9b06ec66573f4aef1e44542fa6b2
Edd Smith <edd@sourcelab.technology>
2017-04-02 17:44:50 +0100

    did another thing

--- Heartrate_intensity.rb
+++ Heartrate_intensity.rb
@@ -1,6 +1,6 @@
 def get_target_heart_rate(intensity)
   target_heart_rate = (((220 - @age) - @resting_heart_rate) * intensity) + @resting_heart_rate
-  print ((intensity*100).to_i).to_s + '%' + '       | ' + target_heart_rate.round.to_s + "\n"
+  print ((intensity*100).to_i).to_s + '%       | ' + target_heart_rate.round.to_s + "\n"
   get_target_heart_rate(intensity + 0.05) if intensity < 0.95
 end
 
