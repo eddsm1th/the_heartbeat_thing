6aaaefbd83a46ed223826d29972edb9fb2540d8b
Edward Smith <eddsm1th@hotmail.com>
2017-06-20 15:12:56 +0100

    compressed AF

--- Heartrate_intensity.rb
+++ Heartrate_intensity.rb
@@ -1,11 +1,10 @@
 def a(b)
-  c=(((220-@e)-@d)*b)+@d
-  puts((b*100).to_i).to_s+'%       | '+c.round.to_s
+  puts ((b*100).to_i).to_s+'%       | '+((((220-@x)-@c)*b)+@c).round.to_s
   a(b+0.05)if b<0.95
 end
 puts'Please enter you age:'
-@e=gets.to_i
+@x=gets.to_i
 puts'Please enter your resting heart rate:'
-@d = gets.to_i
+@c=gets.to_i
 puts"\nIntensity | Target Heart Rate\n----------|--------------"
 a(0.55)
