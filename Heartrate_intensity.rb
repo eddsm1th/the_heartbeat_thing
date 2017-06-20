509f11363cb0734328ba282a2b1c14e2c2d28ba7
Edward Smith <eddsm1th@hotmail.com>
2017-06-20 16:16:25 +0100

    'd'

--- Heartrate_intensity.rb
+++ Heartrate_intensity.rb
@@ -1,8 +1,8 @@
 def a(b)
-p"#{b}%\t  #{((((220-@x)-@c)*b)+@c)/100}"  
+p"#{b}%       #{((((220-@x)-@c)*b)+@c)/100}"  
 a(b+5)if b<95
 end
 p'Enter age and Resting heart rate'
-@x,@c=gets.to_i,gets.to_i
+@x,@c=inp.to_i,inp.to_i
 p'Intensity Target Heart Rate'
 a(55)
