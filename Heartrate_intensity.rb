b3a796a3a9989e37764d948f7a830d4399b2f60f
Edward Smith <eddsm1th@hotmail.com>
2017-06-20 15:28:16 +0100

    d

--- Heartrate_intensity.rb
+++ Heartrate_intensity.rb
@@ -1,8 +1,9 @@
 def a(b)
-  puts ((b*100).to_i).to_s+"%\t  "+((((220-@x)-@c)*b)+@c).round.to_s
+  print (b*100).to_i
+  puts"%\t  "+((((220-@x)-@c)*b)+@c).round.to_s
   a(b+0.05)if b<0.95
 end
-puts'Enter your age:'
+'Enter your age:'
 @x=gets.to_i
 puts'Enter your resting heart rate:'
 @c=gets.to_i
