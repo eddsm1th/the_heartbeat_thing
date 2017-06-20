c96ddca4b477e6e83169e10be3f1bf5557abac4f
Edward Smith <eddsm1th@hotmail.com>
2017-06-20 15:56:43 +0100

    'd'

--- Heartrate_intensity.rb
+++ Heartrate_intensity.rb
@@ -2,9 +2,9 @@ def a(b)
   puts"#{b}%\t  #{((((220-@x)-@c)*b)+@c)/100}"  
   a(b+5)if b<95
 end
-puts'Enter your age'
+puts'Enter age'
 @x=gets.to_i
-puts'Enter your resting heart rate'
+puts'Enter resting heart rate'
 @c=gets.to_i
 puts'Intensity Target Heart Rate'
 a(55)
