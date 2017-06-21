431498c5faac84f5431b4d1ae3ed660a369be51f
Edward Smith <eddsm1th@hotmail.com>
2017-06-21 16:57:59 +0100

    i think im done

--- Heartrate_intensity.rb
+++ Heartrate_intensity.rb
@@ -1,4 +1,3 @@
-a=->b,x,c{p"#{b}%-#{((220-x-c)*b+c)/100}"
-a[b+5,x,c]if b<95}
+a=->b,x,c{p"#{b}%-#{((220-x-c)*b+c)/100}";a[b+5,x,c]if b<95}
 p'Enter age & Resting heart rate'
 a[55,gets.to_i,gets.to_i]
